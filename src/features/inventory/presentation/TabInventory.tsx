9f958fdaf4a4a88ee7e441df6757e75af8cfd182
rafaelartvideo <rafaelartvideo@gmail.com>
2026-09-14 16:40:42 -0300

    Move resumo do estoque para dashboard

--- src/features/inventory/presentation/TabInventory.tsx
+++ src/features/inventory/presentation/TabInventory.tsx
@@ -1,13 +1,1 @@
-import type { ComponentProps } from "react";
-import { TabInventory as InventoryModule } from "./TabInventoryV2";
-import { InventoryOverview } from "./InventoryOverview";
-
-type Props = ComponentProps<typeof InventoryModule>;
-
-export function TabInventory(props: Props) {
-  if (props.routeResourceId) return <InventoryModule {...props} />;
-  return <div className="space-y-5">
-    <InventoryOverview />
-    <InventoryModule {...props} />
-  </div>;
-}
+export { TabInventory } from "./TabInventoryV2";
